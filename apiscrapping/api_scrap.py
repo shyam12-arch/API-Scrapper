82883b6d6d0d7a4cd567899b4ffaa7b43645c9f2
Shyam tyagi <77968605+shyam12-arch@users.noreply.github.com>
2023-05-13 17:21:44 +0530

    Update api_scrap.py

--- apiscrapping/api_scrap.py
+++ apiscrapping/api_scrap.py
@@ -7,39 +7,14 @@ import pandas as pd
 import requests
 import pymysql as sql
 
-# response = requests.get('https://api.themoviedb.org/3/movie/top_rated?api_key=103104b0c1097f8ad3a11f8add24e37d&language=en-US&page=1')
-# response.json()
-# id = []
-# title = []
-# overview = []
-# release_date = []
-# popularity = []
-# vote_average = []
-# vote_count = []
 
-# df = response.json()['results']
-# print(df[0]['title'])
-
-
-# print(vote_avgs)
-
-
-# df = pd.DataFrame(response.json()['results'])[['id','title','overview','release_date','popularity','vote_average','vote_count']]
-# print(df)
-# print(df.head())
-# ----------------------------------------------------------------------------------
-# df1 = pd.DataFrame()  # create empty dataframe
-# df1
-# for i in range(1,4):
-#     response = requests.get('https://api.themoviedb.org/3/movie/top_rated?api_key=103104b0c1097f8ad3a11f8add24e37d&language=en-US&page={i}')
-#     temp_df = pd.DataFrame(response.json()['results'])[['id','title','overview','release_date','popularity','vote_average','vote_count']]
-#     #print(temp_df)
-#     df1 = df1.append(temp_df,ignore_index=True)  # at one time hitting you get 20 movies
-#
-# # df1.shape
-# # print(df1.shape)
-# df1.to_csv('movies.csv')  # for converting into csv file format
-#------------------------------------------------------------------------------------
+id = []
+title = []
+overview = []
+release_date = []
+popularity = []
+vote_average = []
+vote_count = []
 
 for i in range(3):
     response = requests.get('https://api.themoviedb.org/3/movie/top_rated?api_key=103104b0c1097f8ad3a11f8add24e37d&language=en-US&page={i}')
@@ -53,13 +28,13 @@ for i in range(3):
         popularitys = df[i]['popularity']
         vote_averages = df[i]['vote_average']
         vote_counts = df[i]['vote_count']
-        print(ids)
-        print(titles)
-        print(overviews)
-        print(release_dates)
-        print(popularitys)
-        print(vote_averages)
-        print(vote_counts)
+#         print(ids)
+#         print(titles)
+#         print(overviews)
+#         print(release_dates)
+#         print(popularitys)
+#         print(vote_averages)
+#         print(vote_counts)
 
         con = sql.connect(host='localhost', user='root', passwd='Krish@4558$', database='manoj')
         cur = con.cursor()  # create object help in query execution
@@ -67,7 +42,8 @@ for i in range(3):
         # data insertion:
         cur.execute("insert into api values(%s,'%s','%s','%s',%s,%s,%s)"%(ids, titles, overviews, release_dates, popularitys, vote_averages, vote_counts))
         con.commit()  # for making parmanent change in table
-
+        
+--------------------------------------------------------------------------       
 #         id.append(ids)
 #         title.append(titles)
 #         overview.append(overviews)
@@ -75,15 +51,16 @@ for i in range(3):
 #         popularity.append(popularitys)
 #         vote_average.append(vote_averages)
 #         vote_count.append(vote_counts)
-# # ------------------------------------------------------------------------
-# # create dictionary for convert above data into csv file format:
+
+# create dictionary for convert above data into csv file format:
+
 # dictionary = {'id': id, 'title': title, 'overview': overview, 'release_date': release_date, 'popularity': popularity, 'vote_average': vote_average, 'vote_count': vote_count}
 # df = pd.DataFrame(dictionary)
 # to_csv = df.to_csv('/Users/shyamtyagi/api.csv')
-# ------------------------------------------------------------------------
 
+--------------------------------------------------------------------------
 
-# -----------------------------------------------------------------------
+#     for getting perticular col info. :
 
 #     ids = [d['id'] for d in df]
 #     titles = [d['title'] for d in df]
